719ae12685a5f0365e3670a43144edd5281d9cf3
Travis Kent <travisokent@gmail.com>
2019-04-13 15:05:38 -0600

    PlayerHealth

---
+++ Space Shooter/Assets/HealthManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public static HealthManager instance;
+
+    void Awake()
+    {
+        instance =this;
+    }
+
+    public int currentHealth;
+    public int maxHealth;
+
+    public GameObject deathEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HurtPlayer()
+    {
+        currentHealth--;
+
+        if(currentHealth <=0)
+        {
+            Instantiate(deathEffect, transform.position, transform.rotation);
+            gameObject.SetActive(false);
+        }
+    }
+}
